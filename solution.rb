cec9c8aad8666ab3a28c0b7828c55d3d261314d2
Hugo Tamayo <doctamayot@hotmail.com>
2018-09-16 19:40:40 -0500

    subo reto corregido

--- solution.rb
+++ solution.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 
 get '/' do
   unless params[:nombre]
-    "Hola desconocido!."
+    <<-HTML
+    <h1>"Hola desconocido!."</h1>
+    HTML
   else
     "Hola #{params[:nombre]}!"
   end
