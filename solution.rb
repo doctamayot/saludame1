d710ce0472462cd51be648209b0713614ce407eb
Hugo Tamayo <doctamayot@hotmail.com>
2018-09-17 19:36:32 -0500

    con vista

--- solution.rb
+++ solution.rb
@@ -1,13 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  if params[:nombre] == nil
-    <<-HTML
-    <h1>Hola desconocido!</h1>
-    HTML
-  else
-    <<-HTML
-    <h1>Hola #{params[:nombre].capitalize}!</h1>
-    HTML
-  end
+  erb :vista
 end
